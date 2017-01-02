015bbb5bd1673aa553ab4698646a1b9713b0eac5
Oliver Carlos <oliverpascualcarlos@gmail.com>
2017-01-02 20:51:21 +0800

    added node controller restful.

---
+++ node-restful-controller-mongo.js
@@ -0,0 +1,54 @@
+'use strict';
+// requires the author model.
+var Model = require('../models/Model.js');
+
+module.exports = {
+	/**
+	* Index function that list all the resource.
+	*/
+	index : function (req, res, next) {
+
+	},
+
+	/**
+	* Displays the create page of the resource.
+	*/
+	create: function (req, res, next) {
+
+	},
+
+	/**
+	* Stores the new created resource
+	*/
+	store: function (req, res, next) {
+
+	},
+
+	/**
+	* Displays the edit page of the resource.
+	*/
+	edit: function (req, res, next) {
+
+	},
+
+	/**
+	* Updates the new modified resource
+	*/
+	update: function (req, res, next) {
+
+	},
+
+	/**
+	* Deletes the resource
+	*/
+	destroy: function (req, res, next) {
+
+	},
+
+	/**
+	* Restores the resource
+	*/
+	restore: function (req, res, next) {
+
+	},
+};
